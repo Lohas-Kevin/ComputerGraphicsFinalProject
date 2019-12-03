951d9a6075b7f8021d3048adcf09f9fd099f7e59
Lohas-Kevin's Desktop <cheny43@rpi.edu>
2019-12-03 00:47:54 -0500

    finish the instantiate function

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 
 class Spaceship {
     constructor(object, name, type, sceneRef){
-        this.object = object;
+        this.obj = object;
         this.name = name;
         this.type = type;
         this.sceneRef = sceneRef;
@@ -16,14 +16,15 @@ class Spaceship {
     damage(value) {
         this.health -= value;
         if(this.health <= 0){
-            this.sceneRef.remove(this.object);
+            this.sceneRef.remove(this.obj);
         }
     }
 };
 
 var camera, scene, renderer, manager;
 var spaceshipList = [];
-var player = new THREE.Object3D();
+var player;
+var visibleHeight, visibleWidth;
 
 init();
 animate();
@@ -33,7 +34,7 @@ function init(){
     document.body.appendChild(container);
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-    camera.position.z = 120;
+    camera.position.z = 150;
 
     scene = new THREE.Scene();
 
@@ -44,16 +45,30 @@ function init(){
 	camera.add( pointLight );
     scene.add(camera);
 
-    var obj = LoadModel('models/player_spaceship.obj', 'models/player_spaceship.mtl', "Test");
-    scene.add(obj);
-    
+    //var obj = LoadModel('models/player_spaceship.obj', 'models/player_spaceship.mtl');
+    //scene.add(obj);
+    player = CreateElement("Player", "Spaceship", 'models/player_spaceship.obj', 'models/player_spaceship.mtl');
+    player.obj.rotateX(Math.PI / 2);
+    player.obj.rotateY(Math.PI);
 
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(renderer.domElement);
 
+    visibleHeight = visibleHeightAtZDepth(0,camera);
+    visibleWidth = visibleWidthAtZDepth(0, camera);
+
 };
+
+function CreateElement(name, type, ObjURL, MtlURL){
+    var obj = LoadModel(ObjURL, MtlURL);
+    obj.name = name;
+    scene.add(obj);
+    var temp = new Spaceship(obj, name, type, scene);
+    spaceshipList.push(temp);
+    return temp;
+}
 
 
 function animate() {
@@ -62,12 +77,14 @@ function animate() {
     setTimeout(function(){
         requestAnimationFrame( animate );
     }, 1000 / 60)
-	
+    
+    //player.obj.translateX(0.1);
+    //console.log(player.obj.position);
     renderer.render( scene, camera );
     
 };
 
-function LoadModel(ObjURL,MtlURL,name){
+function LoadModel(ObjURL,MtlURL){
     var mLoader = new THREE.MTLLoader();
     var oLoader = new THREE.OBJLoader();
     var result = new THREE.Object3D();
@@ -97,6 +114,28 @@ function LoadModel(ObjURL,MtlURL,name){
     );
     return result;
 };
+
+
+//code from 
+//https://discourse.threejs.org/t/functions-to-calculate-the-visible-width-height-at-a-given-z-depth-from-a-perspective-camera/269
+//this is used to calculate the height and width.
+function visibleHeightAtZDepth( depth, camera ){
+    // compensate for cameras not positioned at z=0
+    var cameraOffset = camera.position.z;
+    if ( depth < cameraOffset ) depth -= cameraOffset;
+    else depth += cameraOffset;
+  
+    // vertical fov in radians
+    var vFOV = camera.fov * Math.PI / 180; 
+  
+    // Math.abs to ensure the result is always positive
+    return 2 * Math.tan( vFOV / 2 ) * Math.abs( depth );
+  };
+  
+function visibleWidthAtZDepth( depth, camera ){
+    var height = visibleHeightAtZDepth( depth, camera );
+    return height * camera.aspect;
+  };
 
 
 
