5d0c9d63f329ccfffb51328bfb82249f397e909a
Lohas-Kevin's Desktop <cheny43@rpi.edu>
2019-12-03 22:46:01 -0500

    Collision Detection is working

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,8 @@ class Spaceship {
         this.sceneRef = sceneRef;
         this.health = 100;
         this.attack = 25;
+        this.canShoot = 0;
+        this.alive = true;
     }
 
     returnName(){
@@ -15,6 +17,7 @@ class Spaceship {
 
     damage(value) {
         this.health -= value;
+        this.alive = false;
         if(this.health <= 0){
             this.sceneRef.remove(this.obj);
         }
@@ -27,7 +30,31 @@ class Spaceship {
     rotateAngle(value){
         this.obj.rotateY(value);
     }
+
+    setBoxRef(box){
+        this.box = box;
+    }
 };
+
+class CollisionDetection{
+    constructor(obj){
+        this.visualization = new THREE.BoxHelper(obj);
+        this.box = new THREE.Box3().setFromObject(obj);
+        scene.add(this.visualization);
+    }
+
+    setRef(ref){
+        this.ref = ref;
+    }
+
+    update(obj){
+        if(obj != undefined){
+            this.visualization.update();
+            this.box = this.box.setFromObject(obj);
+        }
+        
+    }
+}
 
 var camera, scene, renderer, manager;
 var spaceshipList = [];
@@ -37,6 +64,7 @@ var visibleHeight, visibleWidth;
 var clock;
 var keyboard = new THREEx.KeyboardState();
 var bulletList = [];
+var boundingBoxList = [];
 
 init();
 animate();
@@ -57,14 +85,15 @@ function init(){
 	camera.add( pointLight );
     scene.add(camera);
 
-    //var obj = LoadModel('models/player_spaceship.obj', 'models/player_spaceship.mtl');
+    //var obj = LoadModel('models/enemy_spaceship.obj', 'models/enemy_spaceship.mtl');
     //scene.add(obj);
     player = CreateElement("Player", "Spaceship", 'models/player_spaceship.obj', 'models/player_spaceship.mtl');
     player.obj.rotateX(Math.PI / 2);
     player.obj.rotateY(Math.PI);
 
-    player2 = CreateElement("Player2", "Spaceship", 'models/enemy_ship1.obj', 'models/enemy_ship1.mtl');
+    player2 = CreateElement("Player2", "Spaceship", 'models/player_spaceship.obj', 'models/player_spaceship.mtl');
     player2.obj.rotateX(Math.PI/2);
+    
 
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -74,7 +103,7 @@ function init(){
     visibleHeight = visibleHeightAtZDepth(0,camera);
     visibleWidth = visibleWidthAtZDepth(0, camera);
 
-    //player.obj.translateZ(-visibleHeight/2 + 10);
+    player.obj.translateZ(-visibleHeight/2 + 10);
     player2.obj.translateZ(-visibleHeight/2 + 10);
 
     clock = new THREE.Clock();
@@ -85,8 +114,16 @@ function CreateElement(name, type, ObjURL, MtlURL){
     var obj = LoadModel(ObjURL, MtlURL);
     obj.name = name;
     scene.add(obj);
+
     var temp = new Spaceship(obj, name, type, scene);
+    var box = new CollisionDetection(temp.obj);
+    temp.setBoxRef(box);
+    box.setRef(temp);
+    
     spaceshipList.push(temp);
+    boundingBoxList.push(box);
+
+    
     return temp;
 }
 
@@ -173,6 +210,31 @@ function update(){
         }
 
         bulletList[index].position.add(bulletList[index].velocity);
+        bulletList[index].box.update(bulletList[index]);
+
+        //check the bullet collection
+        if(bulletList[index].from == "Player" && 
+            bulletList[index].box.intersect == player2.box){
+                console.log('hit');
+        };
+        console.log(bulletList[index].box.box.getCenter());
+        console.log(bulletList[index].position);
+
+        if(bulletList[index].from == "Player" && 
+            bulletList[index].box.box.intersectsBox(player2.box.box) == true){
+            console.log('hit');
+        }
+    }
+
+    for(var index = 0; index < boundingBoxList.length; index += 1){
+        if(boundingBoxList[index] === undefined){
+            continue;
+        }
+        if(boundingBoxList[index].ref.alive = false){
+            boundingBoxList.splice(index, 1);
+            continue;
+        }
+        boundingBoxList[index].update(boundingBoxList[index].ref.obj);
     }
 
     if(keyboard.pressed('W')){
@@ -188,8 +250,9 @@ function update(){
         player.rotateAngle(-rotationAngle)
     }
 
-    if(keyboard.pressed('space')){
-        shoot(player.obj.position,player.obj.rotation);
+    if(keyboard.pressed('space') && player.canShoot <= 0){
+        shoot(player.obj.position,player.obj.rotation, 0xffff00, "Player");
+        player.canShoot = 10;
     }
 
     //console.log(player.obj.rotation);
@@ -206,12 +269,22 @@ function update(){
     if(keyboard.pressed('right')){
         player2.rotateAngle(-rotationAngle)
     }
+
+    if(player.canShoot > 0){
+        player.canShoot -= 1;
+    }
+
 };
 
-function shoot(pos,rot){
+function shoot(pos,rot, colorNum, from){
     var geometry = new THREE.SphereGeometry(0.5, 8, 8);
-    var material = new THREE.MeshBasicMaterial({color: 0xffff00});
+    var material = new THREE.MeshBasicMaterial({color: colorNum});
     var bullet = new THREE.Mesh(geometry, material);
+    
+    var box = new CollisionDetection(bullet);
+    bullet.box = box;
+    bullet.from = from;
+    box.setRef(bullet);
 
     if(rot.x <= 0){
         bullet.position.set(pos.x + 10*Math.sin(rot.y), pos.y + 10*Math.cos(rot.y), pos.z);
@@ -234,14 +307,18 @@ function shoot(pos,rot){
     bullet.alive = true;
     setTimeout(
         function(){
-            bullet.alive = false;
-            scene.remove(bullet);
+            if(bullet != undefined){
+                bullet.alive = false;
+                scene.remove(bullet);
+                scene.remove(bullet.box.visualization);
+            }
+            
         },
-        1000
+        1250
     );
 
     scene.add(bullet);
-    bulletList.push(bullet)
+    bulletList.push(bullet);
 }
 
 
